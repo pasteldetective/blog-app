3c44aacaee46de0fbbe4dde2e4ff810340ed4ff6
Jackie Yee <pi.jackyy@gmail.com>
2024-04-25 20:42:43 -0400

    started header and herosection of blog 10:53

---
+++ app/home/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {}
+
+const HeroSection = (props: Props) => {
+    return (
+        <section></section>
+    )
+}
+
+export default HeroSection
